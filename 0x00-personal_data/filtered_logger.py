d6d77ba1f6ee126040e4116a02a1a557cc313337
bravin-onwonga <onbravin@gmail.com>
2024-09-01 01:33:20 +0300

    Annonating my function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Filter using re.sub"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
     """filters data and returns a message"""
     for key in fields:
         message = re.sub(rf"{key}=[^{separator}]*",
