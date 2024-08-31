b357b110319970ec4c6eeab73340acbd0e65911e
bravin-onwonga <onbravin@gmail.com>
2024-09-01 02:09:23 +0300

    Filter values of a record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Filter using re.sub"""
 import re
-from typing import List
+from typing import List, Tuple
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf"{key}=[^{separator}]*",
                          f'{key}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str]) -> None:
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """"filter values in incoming log records"""
+        fields = [key for key in self.fields]
+        return filter_datum(fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
