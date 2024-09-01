53324541bf7c52150283ed14189f8fb73a8b59c5
bravin-onwonga <onbravin@gmail.com>
2024-09-01 03:30:00 +0300

    Annonating my function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,8 +6,16 @@ import bcrypt
 
 def hash_password(password: str) -> str:
     """Uses hashpw to hash a password"""
-    password_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(password_bytes, salt)
+    password_bytes: bytes = password.encode('utf-8')
+    salt: bytes = bcrypt.gensalt()
+    hash: bytes = bcrypt.hashpw(password_bytes, salt)
 
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if password is similar to hashed_password"""
+    hash: bytes = hash_password(password)
+    if hashed_password == hash:
+        return True
+    return False
