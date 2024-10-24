fa70b7c0d295213274617ff44176f4ac5509c417
bravin-onwonga <onbravin@gmail.com>
2024-10-24 10:23:58 +0300

    chore:In memory session management

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """ Session authentication class """
 from api.v1.auth.auth import Auth
+from uuid import uuid4 as uid
 
 
 class SessionAuth(Auth):
     """ Sessions class """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a new session """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        key = uid()
+        self.user_id_by_session_id[key] = user_id
+        return key        
