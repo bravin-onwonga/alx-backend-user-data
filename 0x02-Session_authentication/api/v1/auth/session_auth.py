f2d8d26247d9bd18421d719c937245a4804d59f9
bravin-onwonga <onbravin@gmail.com>
2024-10-24 12:32:34 +0300

    fix:Fixing styling errors

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,11 +7,11 @@ from uuid import uuid4 as uid
 class SessionAuth(Auth):
     """ Sessions class """
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """ Create a new session """
         if not user_id or not isinstance(user_id, str):
             return None
         key = uid()
         self.user_id_by_session_id[key] = user_id
-        return key        
+        return key    
